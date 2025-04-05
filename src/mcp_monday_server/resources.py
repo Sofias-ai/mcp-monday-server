f9536be82cd04a4caa13c6e19f4bdc2ed2e68d13
sssSofiaS <sss@sofias.ai>
2025-04-05 09:59:02 +0200

    Update resources descriptions

--- src/mcp_monday_server/resources.py
+++ src/mcp_monday_server/resources.py
@@ -2,9 +2,7 @@ from typing import Dict, Any, Union
 from .common import logger, monday_client
 
 def get_board_schema(board_id: Union[int, str]) -> Dict[str, Any]:
-    """
-    Get schema of a specific board.
-    """
+    #  Get schema of a specific board.
     logger.info(f"Getting schema from board {board_id}")
     
     board_data = monday_client.boards.fetch_boards_by_id(board_id)
@@ -21,15 +19,11 @@ def get_board_schema(board_id: Union[int, str]) -> Dict[str, Any]:
     return result
 
 def get_item_by_id(item_id: Union[int, str]) -> Dict[str, Any]:
-    """
-    Get item details by item ID.
-    """
+    # Get item details by item ID.
     logger.info(f"Getting item with ID {item_id}")
     return monday_client.items.fetch_items_by_id(item_id)
 
 def get_items_by_column_value(board_id: Union[int, str], column_id: Union[int, str], column_value: str) -> Dict[str, Any]:
-    """
-    Get items by column value.
-    """
+    # Get items by column value. 
     logger.info(f"Getting items from board {board_id} with column {column_id} and value {column_value}")
     return monday_client.items.fetch_items_by_column_value(board_id, column_id, column_value)
